dfa8ce05c51eec122d09f627c94f9d38b6320792
khs96523 <khs96523@gmail.com>
2024-07-12 23:40:38 +0900

    FEAT: Practice Persistence Context

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -22,7 +22,6 @@ public class JpaMain {
 //            member.setId(2L);
 //            member.setName("HelloB");
 //            em.persist(member);
-//            em.persist(member);
 
             /* 조회 */
 //            Member findMember = em.find(Member.class, 1L);
@@ -34,14 +33,39 @@ public class JpaMain {
 //            em.remove(findMember);
 
             /* JPQL */
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            /* 비영속 */
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+
+            /* 영속 */
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+
+            /* 1차 캐싱 조회, 영속 엔티티의 동일성 보장 */
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+            /* 쓰기 지연 */
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "A");
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("=========================");
+
+            /* 변경 감지 */
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
 
             tx.commit();
         } catch (Exception e) {
